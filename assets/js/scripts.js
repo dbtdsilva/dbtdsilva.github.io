d7c84d0e9c87a9f1899bc4bd85b89b0f4df484cd
Diogo Silva <dbtds@ua.pt>
2016-11-27 01:22:52 +0000

    Modified magnific popup and added some effects

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,26 +2,12 @@
 Theme Name: IAMX
 Author: Trendy Theme
 Author URL: trendytheme.net
+
+Slighty modified..
 */
 
-/*
-    = Preloader
-    = Animated scrolling / Scroll Up
-    = Full Screen Slider
-    = Sticky Menu
-    = Back To Top
-    = Countup
-    = Progress Bar
-    = More skill
-    = Shuffle
-    = Magnific Popup
-    = Vidio auto play
-    = Fit Vids
-    = Google Map
 
-*/
-
-jQuery(function ($) {
+jQuery(function($) {
 
     'use strict';
 
@@ -38,8 +24,8 @@ jQuery(function ($) {
     // Animated scrolling / Scroll Up
     // -------------------------------------------------------------
 
-    (function () {
-        $('a[href*=#]').bind("click", function(e){
+    (function() {
+        $('a[href*=#]').bind("click", function(e) {
             var anchor = $(this);
             $('html, body').stop().animate({
                 scrollTop: $(anchor.attr('href')).offset().top
@@ -53,10 +39,10 @@ jQuery(function ($) {
     // -------------------------------------------------------------
     // Full Screen Slider
     // -------------------------------------------------------------
-    (function () {
+    (function() {
         $(".tt-fullHeight").height($(window).height());
 
-        $(window).resize(function(){
+        $(window).resize(function() {
             $(".tt-fullHeight").height($(window).height());
         });
 
@@ -67,7 +53,7 @@ jQuery(function ($) {
     // Sticky Menu
     // -------------------------------------------------------------
 
-    (function () {
+    (function() {
         $('.header').sticky({
             topSpacing: 0
         });
@@ -79,13 +65,11 @@ jQuery(function ($) {
     }());
 
 
-
-
     // -------------------------------------------------------------
     // Back To Top
     // -------------------------------------------------------------
 
-    (function () {
+    (function() {
         $(window).scroll(function() {
             if ($(this).scrollTop() > 100) {
                 $('.scroll-up').fadeIn();
@@ -99,7 +83,7 @@ jQuery(function ($) {
     // Shuffle
     // -------------------------------------------------------------
 
-    (function () {
+    (function() {
 
         var $grid = $('#grid');
 
@@ -107,36 +91,14 @@ jQuery(function ($) {
             itemSelector: '.portfolio-item'
         });
 
-        /* reshuffle when user clicks a filter item */
-        $('#filter a').click(function (e) {
+        $('#filter a').click(function(e) {
             e.preventDefault();
-
-            // set active class
             $('#filter a').removeClass('active');
             $(this).addClass('active');
-
-            // get group name from clicked item
             var groupName = $(this).attr('data-group');
-
-            // reshuffle grid
-            $grid.shuffle('shuffle', groupName );
+            $grid.shuffle('shuffle', groupName);
         });
 
-
-    }());
-
-
-    // -------------------------------------------------------------
-    // Magnific Popup
-    // -------------------------------------------------------------
-
-    (function () {
-      $('.image-link').magnificPopup({
-        closeBtnInside: true,
-        removalDelay: 300, // Delay in milliseconds before popup is removed
-        mainClass: 'mfp-with-zoom', // this class is for CSS animation below
-        type: 'inline'
-      });
 
     }());
 
@@ -144,10 +106,10 @@ jQuery(function ($) {
     // WOW JS
     // -------------------------------------------------------------
 
-    (function () {
+    (function() {
 
         new WOW({
-            mobile:  false
+            mobile: false
         }).init();
 
     }());
@@ -158,7 +120,7 @@ jQuery(function ($) {
     // Contact Form
     // -------------------------------------------------------------
 
-    $('#contactForm').on('submit',function(e){
+    $('#contactForm').on('submit', function(e) {
 
         e.preventDefault();
 
@@ -168,16 +130,16 @@ jQuery(function ($) {
 
         $this.prevAll('.alert').remove();
 
-        $.post( $action, $data, function( data ) {
+        $.post($action, $data, function(data) {
 
-            if( data.response=='error' ){
+            if (data.response == 'error') {
 
-                $this.before( '<div class="alert alert-danger">'+data.message+'</div>' );
+                $this.before('<div class="alert alert-danger">' + data.message + '</div>');
             }
 
-            if( data.response=='success' ){
+            if (data.response == 'success') {
 
-                $this.before( '<div class="alert alert-success">'+data.message+'</div>' );
+                $this.before('<div class="alert alert-success">' + data.message + '</div>');
                 $this.find('input, textarea').val('');
             }
 
@@ -185,10 +147,30 @@ jQuery(function ($) {
 
     });
 
+    // Inline popups
+    $('#grid').magnificPopup({
+        delegate: 'a',
+        removalDelay: 100, //delay removal by X to allow out-animation
+        callbacks: {
+            beforeOpen: function() {
+                this.st.mainClass = this.st.el.attr('data-effect');
+            }
+        },
+        midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
+    });
 
+    // Hinge effect popup
+    $('a.hinge').magnificPopup({
+        mainClass: 'mfp-with-fade',
+        removalDelay: 1000, //delay removal by X to allow out-animation
+        callbacks: {
+            beforeClose: function() {
+                this.content.addClass('hinge');
+            },
+            close: function() {
+                this.content.removeClass('hinge');
+            }
+        },
+        midClick: true
+    });
 });
-
-
-
-
-
